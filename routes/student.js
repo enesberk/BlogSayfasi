d5e5e7869f5f747d2880058f326eb4e5e0a7f632
enesberk <180403049@ogr.gelisim.edu.tr>
2022-05-13 02:45:59 +0300

    update route added and add route fixed

--- routes/student.js
+++ routes/student.js
@@ -6,8 +6,43 @@ const studentController = new StudentController()
 router.get('/', async (req, res) => {
     const students = await studentController.GetAll()
     res.render('index', {
-        students: students
+        students
     })
+})
+
+router.post('/detail', async (req, res) => {
+    try {
+        const detail = await studentController.GetById(req.body.id)
+        if (!detail) return res.status(444).json({
+            message: 'Error'
+        })
+        return res.status(200).json({
+            message: 'OK',
+            detail
+        })
+    } catch (error) {
+        return res.status(444).json({
+            message: 'Hata'
+        })
+    }
+})
+
+router.put('/update', async (req, res) => {
+    try {
+        const {id, name, no, className} = req.body
+        const result = await studentController.UpdateById(id, {name, no, className})
+        if (!result) return res.status(444).json({
+            message: 'Error'
+        })
+        return res.status(200).json({
+            message: 'OK',
+            result
+        })
+    } catch (error) {
+        return res.status(444).json({
+            message: 'Hata'
+        })
+    }
 })
 
 module.exports = router
